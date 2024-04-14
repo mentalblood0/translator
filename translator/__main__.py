09977096b30f81c5c8d59d123a1a1c89e194c982
mentalblood <neceporenkostepan@gmail.com>
2024-04-14 14:01:07 +0300

    add language choices list

--- translator/__main__.py
+++ translator/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+import subprocess
 import sys
 
 from .Pdf import Pdf
@@ -6,7 +7,12 @@ from .Translator import Translator
 
 parser = argparse.ArgumentParser(prog="Translator", description="Translate PDF and generate sentencewise table")
 parser.add_argument("-f", "--format", required=True, choices=["markdown", "html", "json"])
-parser.add_argument("-l", "--language", required=True)
+parser.add_argument(
+    "-l",
+    "--language",
+    required=True,
+    choices=subprocess.run(args=["trans", "-list-codes"], capture_output=True).stdout.decode().split("\n"),
+)
 args = parser.parse_args()
 
 pdf = Pdf(sys.stdin.buffer.read())
